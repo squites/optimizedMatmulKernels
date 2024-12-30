da4aaa33e38bfb2e1982b8b1e0a575b77f1151c6
squites <samuelqlopes99@gmail.com>
2024-12-30 02:56:10 -0300

    load into SMEM and not doing dotprod yet

--- kernel_3.cuh
+++ kernel_3.cuh
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <iostream>
 
+
+//__device__ void load_smem();
+
+/*
 #define TILE_SIZE 32 // tile = 32x32 = 1024
-
-__device__ void load_smem();
-
-
 // A=(M,k), B=(k,N)
 __global__ void gemm_smem_cache_blocking_k(const float *A, const float *B, float *C, size_t m, size_t n, size_t k,) {
     // allocate shared memory
@@ -57,6 +57,8 @@ __global__ void gemm_smem_cache_blocking_k(const float *A, const float *B, float
 // 1 - load chunk of A and chunk of B from GMEM to SMEM
 // 2 - work of these chunks
 // move the chunks along the row and column
+*/
+
 
 #define CHUNK_SIZE 32
 __global__ void gemm_smem_cache_blocking_v2_k(const float *A, const float *B, float *C, 
@@ -78,15 +80,23 @@ __global__ void gemm_smem_cache_blocking_v2_k(const float *A, const float *B, fl
     // row_idx * width: pula para o começo da linha correspondente com o indice.
     // + CHUNKSIZE: chega no primeiro elemento to próximo chunk na mesma linha.
 
-    int nchunks = blockDim.y/CHUNK_SIZE;
-    // loop can be iterating over tiles or over elements.
-    for (int bi = 0; bi < nchunks; bi++) {
-        for (int ki = 0; ki < CHUNK_SIZE; ki++) { // need to check this loop
-            // load A and B chunks into shared memory
-            A_shared[threadIdx.y * CHUNK_SIZE + threadIdx.x] = A_ptr[c_row * k + c_col];
-            B_shared[threadIdx.y * CHUNK_SIZE + threadIdx.x] = B_ptr[c_row * n + c_col];
+    // loop over tiles
+    float sum = 0.0f;
+    const int nchunks = k/CHUNK_SIZE;
+    // while (chunk < nchunks) {
+    for (int chunki = 0; chunki < nchunks; chunki++) { // (each thread will eventually load one element of each chunk)
+        // thread loads element of A and B chunks into SMEM. All threads are doing the same thing on different data at "same" time
+        A_shared[threadIdx.y * CHUNK_SIZE + threadIdx.x] = A_ptr[c_row * k + c_col]; 
+        B_shared[threadIdx.y * CHUNK_SIZE + threadIdx.x] = B_ptr[c_row * n + c_col];
+        __syncthreads();
+
+        // barrier?
+        // calculate dot-product (at this point, all threads of that block loaded the corresponded element of that chunk into shared, so we can already do matmul on this tile)
+        for (int i = 0; i < CHUNK_SIZE*CHUNK_SIZE; i++) { // should be CHUNK_SIZE*CHUNK_SIZE ?
+            sum += A_shared[c_row * CHUNK_SIZE + i] * B_shared[i * n + c_col]
         }
         __syncthreads();
+        C_ptr[c_row * n + c_col] = sum;
     }
 
 }
@@ -99,6 +109,19 @@ __global__ void gemm_smem_cache_blocking_v2_k(const float *A, const float *B, fl
 // Para computar cada resultado parcial do tile C, em cada iteração, o thread block computa uma matmul entre 
 // o tile A e o tile B. Com isso ele tem um resultado parcial do tile C. faz isso até acabar os tiles daquela tilerow
 // e daquela tilecol.
+//
+// OBS 2 (IMPORTANTE): o loop itera por cada chunk. O porquê disso é que um thread_block é responsável por computar
+// um tile de C. Para isso, ele precisa calcular uma tileRow inteira de A e uma tileCol inteira de B, e para isso,
+// é necessário carregar chunks de A e de B para a shared memory. Só que pensa comigo, o ideal seria colocar toda a
+// tileRow e tileCol na shared_memory, só que não cabe, pois ela comporta poucos dados. Então, em cada iteração 
+// carregamos um chunk de A e um chunk de B, onde cada thread desse thread_block carrega apenas 1 elemento do chunk
+// A e um elemento do chunk B. Porém, devemos lembrar que para completar a tileRow e tileCol inteira, é necessário 
+// carregar vários chunks para a shared memory. Com isso, a mesma thread que carregou um elemento específico do chunk
+// A e do chunk B para a shared_memory, será também responsável de carregar os "mesmos" elementos correspondentes
+// de cada próximo chunk. Por isso o loop itera por chunks e não por elementos. 
+// Devemos pensar que o kernel roda em uma thread, então precisamos pensar como uma thread. Dessa forma, no loop só 
+// é feito um load de A e um load de B em cada iteração, porém, cada thread do block estará fazendo a mesma coisa
+// porém de coordenadas diferentes!
 
 // mapping thread to gmem = threadIdx.y * blockDim.x + threadIdx.x;
 // mapping block  to gmem = blockIdx.y * gridDim.x + blockIdx.x;
@@ -110,4 +133,10 @@ __global__ void gemm_smem_cache_blocking_v2_k(const float *A, const float *B, fl
 - each thread of that block will load one element of tile A and one of tile B
 - shared memory: shared across all threads of the same block
 - shared memory will load an entire row of tiles and col of tiles
+- (reminder) blockIdx.x * blockDim.x + threadIdx.x : global thread ID in x dimension
+- number of blocks in a grid: gridDim.x * gridDim.y
+- number of threads in a block: blockDim.x * blockDim.y * blockDim.z
+- full global thread ID in 2D: 
+    x = blockIdx.x * blockDim.x + threadIdx.x
+    y = blockIdx.y * blockDim.y + threadIdx.y
 */
